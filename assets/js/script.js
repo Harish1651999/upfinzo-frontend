dd2f9a4912a40fee8ab89a92157f5ff04c2bbb16
Harish1651999 <hk1651999@gmail.com>
2024-08-28 12:08:06 +0530

    add style changes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
   $(".testimonials__cards").slick({
     centerMode: true, // Enables center mode
-    centerPadding: "50px", // Adjust the padding around centered slide
+    centerPadding: "60px", // Adjust the padding around centered slide
     slidesToShow: 3, // Number of slides to show
+    slidesToScroll: 1, // Number of slides to scroll
     autoplay: true, // Enable autoplay
-    autoplaySpeed: 3000, // Slide change speed (in milliseconds)
+    autoplaySpeed: 5000, // Slide change speed (in milliseconds)
+    speed: 300,
     pauseOnHover: true, // Pauses autoplay on hover
     arrows: false, // Display next/prev arrows
     dots: false, // Show dots for navigation (optional)
